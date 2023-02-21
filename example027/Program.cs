e5080fc7afbda4e2635ab42f911430bb69bec944
dkarmm <dartikarmeev@mail.ru>
2023-02-21 20:14:45 +0500

    create example027.

---
+++ example027/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число и 
+// выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetNumber ()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int GetResult (int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        result = result + lastDigit;
+        number /= 10;
+    }
+    return result;
+}
+int result = GetResult(GetNumber());
+Console.WriteLine(result);
